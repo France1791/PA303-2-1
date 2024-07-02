b2404b30fc8a6925df965b3c4ee6baed8b1b8851
France Point Du Jour <107435377+France1791@users.noreply.github.com>
2024-07-02 11:15:25 -0400

    initializing the repository for lab303-2-1

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        //    Write a program that declares two integer variables,
+//    assigns an integer to each, and adds them together.
+//    Assign the sum to a variable. Print out the result.
+
+        int x = 1;
+        int y = 1;
+        int z = x + y;
+        System.out.println(z);
+
+        //Write a program that declares two double variables, assigns a number to each,
+// and adds them together. Assign the sum to a variable. Print out the result.
+
+    }
+
+}
