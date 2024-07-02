bc36a4a5c4fb81945354fc3a7840af8afc2108e0
France Point Du Jour <107435377+France1791@users.noreply.github.com>
2024-07-02 11:32:44 -0400

    create program to add ints and doubles

--- src/Main.java
+++ src/Main.java
@@ -1,18 +1,30 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        //    Write a program that declares two integer variables,
-//    assigns an integer to each, and adds them together.
-//    Assign the sum to a variable. Print out the result.
+//        System.out.println("Hello world!");
+        sumInt();
+        sumDouble();
 
+
+    }
+//    Write a program that declares two integer variables, assigns an integer to each,
+//    and adds them together. Assign the sum to a variable
+    static void sumInt(){
         int x = 1;
         int y = 1;
         int z = x + y;
         System.out.println(z);
-
-        //Write a program that declares two double variables, assigns a number to each,
-// and adds them together. Assign the sum to a variable. Print out the result.
-
     }
+
+//    Write a program that declares two double variables, assigns a number to each,
+//    and adds them together. Assign the sum to a variable.
+  static void sumDouble(){
+        double i = 3.5;
+        double j = 1.5;
+        double k = i + j;
+      System.out.println(k);
+  }
+
+//  Write a program that declares an integer variable and a double variable, assigns numbers to each,
+//  and adds them together. Assign the sum to a variable.  Print out the result. What variable type must the sum be?
 
 }
